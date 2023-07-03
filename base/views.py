6381ba71c02422e29422b31dee94f22d8b741d44
Donald Brooks <brooksd079@gmail.com>
2023-07-03 09:17:14 +0300

    url routing systems with endpoints

--- base/views.py
+++ base/views.py
@@ -4,4 +4,9 @@ from django.http import JsonResponse
 # Create your views here.
 
 def getRoutes(request):
+    routes = [
+        '/api/products/',
+        '/api/products/create/',
+        '/api/products/upload/',
+    ]
     return JsonResponse('Hello Brooks', safe=False)
