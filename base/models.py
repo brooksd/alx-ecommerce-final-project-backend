577182d9f2c8a246a758dc6912f4b3fbe6cfb584
Donald Brooks <brooksd079@gmail.com>
2023-07-04 06:20:53 +0300

    Update: Order  model created on the backend

--- base/models.py
+++ base/models.py
@@ -29,3 +29,19 @@ class Review(models.Model):
     
     def __str__(self):
         return str(self.rating)
+    
+class Order(models.Models):
+    user = user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    paymentMethod = models.CharField(max_length=200, null=True, blank=True) 
+    taxPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    shippingPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    totalPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    isPaid = models.BooleanField(default=False) 
+    paidAt = models.DateField(auto_now_add=False, null=True, blank=True)
+    isDelivered = models.BooleanField(default=False)
+    deliveredAt = models.DateField(auto_now_add=True)
+    createdAt = models.DateField(auto_now_add=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+    
+    def __str__(self):
+        return str(self.createdAt)
