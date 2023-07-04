b01901e801654df789da34e7c4e37c801242f391
Donald Brooks <brooksd079@gmail.com>
2023-07-04 06:06:59 +0300

    Update: Review  model created on the backend

--- base/models.py
+++ base/models.py
@@ -15,3 +15,17 @@ class Product(models.Model):
     countInStock = models.IntegerField(null=True, blank=True, default=0)
     createdAt = models.DateField(auto_now_add=True)
     _id = models.AutoField(primary_key=True, editable=False)
+    
+    def __str__(self):
+        return self.name
+    
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True) 
+    rating = models.IntegerField(null=True, blank=True, default=0)
+    comment = models.TextField(null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+    
+    def __str__(self):
+        return str(self.rating)
