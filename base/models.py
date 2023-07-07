11302be4b0798eca3a872d991333f59d740bd83f
Donald Brooks <brooksd079@gmail.com>
2023-07-07 05:34:00 +0300

    Update: Order item class model created

--- base/models.py
+++ base/models.py
@@ -30,8 +30,8 @@ class Review(models.Model):
     def __str__(self):
         return str(self.rating)
     
-class Order(models.Models):
-    user = user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     paymentMethod = models.CharField(max_length=200, null=True, blank=True) 
     taxPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     shippingPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
@@ -45,3 +45,16 @@ class Order(models.Models):
     
     def __str__(self):
         return str(self.createdAt)
+    
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    qty = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    image = models.CharField(max_length=200, null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+    
+    def __str__(self):
+        return str(self.name)
+    
