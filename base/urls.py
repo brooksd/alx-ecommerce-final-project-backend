505688fe0fff94ebc8ae2c31cbd0529321e274b2
Donald Brooks <brooksd079@gmail.com>
2023-07-13 04:00:51 +0300

    django REST Framework jwt installed

--- base/urls.py
+++ base/urls.py
@@ -6,7 +6,7 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('/users/login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.getRoutes, name='routes'),
     path('products/', views.getProducts, name='products'),
     path('products/<str:pk>/', views.getProduct, name='product')
